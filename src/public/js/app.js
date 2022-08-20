0cf0dcb4f05e2271a7fedb762b66dc727ae63dad
Bongjo Ki <bongjo@starshell.co>
2022-08-21 06:56:04 +0900

    Feat: 클라 - 유저가 지정한 닉네임을 채팅에 적용

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,33 +1,55 @@
 const socket = io();
 
-const welcome = document.querySelector('#welcome');
+const welcome = document.getElementById('welcome');
 const form = welcome.querySelector('form');
-const room = document.querySelector('#room');
+const room = document.getElementById('room');
 
 room.hidden = true;
+
 let roomName;
 
-function addMessage(msg) {
+function addMessage(message) {
   const ul = room.querySelector('ul');
   const li = document.createElement('li');
-  li.innerText = msg;
+  li.innerText = message;
   ul.appendChild(li);
 }
-function handleSubmitRoom(event) {
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector('#msg input');
+  const value = input.value;
+  socket.emit('new_message', input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = '';
+}
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector('h3');
+  h3.innerText = `Room ${roomName}`;
+  const msgForm = room.querySelector('#msg');
+  msgForm.addEventListener('submit', handleMessageSubmit);
+}
+
+function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector('input');
-  socket.emit('enter_room', input.value, () => {
-    welcome.hidden = true;
-    room.hidden = false;
-    const title = room.querySelector('h3');
-    title.innerText = `Room: ${roomName}`;
-  });
+  socket.emit('enter_room', input.value, showRoom);
   roomName = input.value;
   input.value = '';
 }
 
-form.addEventListener('submit', handleSubmitRoom);
+form.addEventListener('submit', handleRoomSubmit);
 
-socket.on('welcome', () => {
-  addMessage('Someone joined!');
+socket.on('welcome', (user) => {
+  addMessage(`${user} arrived!`);
 });
+
+socket.on('bye', (left) => {
+  addMessage(`${left} left ㅠㅠ`);
+});
+
+socket.on('new_message', addMessage);
