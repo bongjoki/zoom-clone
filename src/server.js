e0edd80616ee31b938bacf7a3e91c14a18eab771
Bongjo Ki <bongjo@starshell.co>
2022-08-20 17:23:12 +0900

    Feat: 서버 - 유저가 지정한 닉네임을 채팅에 적용

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import SocketIo from 'socket.io';
+import SocketIO from 'socket.io';
 import express from 'express';
 
 const app = express();
@@ -7,24 +7,52 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.render('home'));
-app.get('/*', (req, res) => res.redirect('/'));
-
-const handleListen = () => console.log('Listening on http://localhost:3000');
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIo(httpServer);
+const wsServer = SocketIO(httpServer);
 
 wsServer.on('connection', (socket) => {
-  // Socket middleware
+  socket['nickname'] = 'Anon';
   socket.onAny((event) => {
-    console.log(`Socket Event:${event}`);
+    console.log(`Socket Event: ${event}`);
   });
   socket.on('enter_room', (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit('welcome');
+    socket.to(roomName).emit('welcome', socket.nickname);
   });
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) => socket.to(room).emit('bye', socket.nickname));
+  });
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
+    done();
+  });
+  socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
 });
 
+/*
+const wss = new WebSocket.Server({ server });
+const sockets = [];
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+}); */
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
